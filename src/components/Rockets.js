dc0d0b056e84d5589813ba53fa17dfda44da906b
LimaSahibzada <lima.sahibzada2023@gmail.com>
2024-09-08 00:24:13 +0430

    added new color

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,27 +1,43 @@
-// src/components/Rockets.js
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { reserveRocket, cancelRocket } from '../redux/rocketsSlice';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setRockets, reserveRocket, cancelReservation } from '../redux/rockets/rocketsSlice';
+import RocketsItem from './RocketsItem';
 
 const Rockets = () => {
-  const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
+  const rockets = useSelector((state) => state.rockets.rockets);
+
+  useEffect(() => {
+    const fetchRockets = async () => {
+      const response = await fetch('https://api.spacexdata.com/v4/rockets');
+      const data = await response.json();
+      dispatch(setRockets(data));
+    };
+
+    fetchRockets();
+  }, [dispatch]);
+
+  const handleReserve = (id) => {
+    dispatch(reserveRocket(id));
+  };
+
+  const handleCancel = (id) => {
+    dispatch(cancelReservation(id));
+  };
 
   return (
-    <div>
-      <h2>Available Rockets</h2>
-      {rockets.map((rocket) => (
-        <div key={rocket.id}>
-          <h3>{rocket.rocket_name}</h3>
-          <p>{rocket.description}</p>
-          <button
-            onClick={() => rocket.reserved ? dispatch(cancelRocket(rocket.id)) : dispatch(reserveRocket(rocket.id))}
-          >
-            {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
-          </button>
-          {rocket.reserved && <span>Reserved</span>}
-        </div>
-      ))}
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Rockets</h1>
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {rockets.map((rocket) => (
+          <RocketsItem
+            key={rocket.id}
+            rocket={rocket}
+            onReserve={() => handleReserve(rocket.id)}
+            onCancel={() => handleCancel(rocket.id)}
+          />
+        ))}
+      </div>
     </div>
   );
 };
