0e4b72949f5e17e1331fc8736fded5944e1eb764
LimaSahibzada <lima.sahibzada2023@gmail.com>
2024-09-08 00:44:23 +0430

    solved rockets.js error

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,45 +1,31 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { setRockets, reserveRocket, cancelReservation } from '../redux/rockets/rocketsSlice';
-import RocketsItem from './RocketsItem';
+// src/redux/rockets/RocketsSlice.js
+import { createSlice } from '@reduxjs/toolkit';
 
-const Rockets = () => {
-  const dispatch = useDispatch();
-  const rockets = useSelector((state) => state.rockets.rockets);
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState: {
+    rockets: [],
+    reservedRockets: [],
+  },
+  reducers: {
+    setRockets: (state, action) => {
+      state.rockets = action.payload;
+    },
+    reserveRocket: (state, action) => {
+      const rocketId = action.payload;
+      if (!state.reservedRockets.includes(rocketId)) {
+        state.reservedRockets.push(rocketId);
+      }
+    },
+    cancelReservation: (state, action) => {
+      const rocketId = action.payload;
+      state.reservedRockets = state.reservedRockets.filter(id => id !== rocketId);
+    },
+  },
+});
 
-  useEffect(() => {
-    const fetchRockets = async () => {
-      const response = await fetch('https://api.spacexdata.com/v4/rockets');
-      const data = await response.json();
-      dispatch(setRockets(data));
-    };
+// Export actions
+export const { setRockets, reserveRocket, cancelReservation } = rocketsSlice.actions;
 
-    fetchRockets();
-  }, [dispatch]);
-
-  const handleReserve = (id) => {
-    dispatch(reserveRocket(id));
-  };
-
-  const handleCancel = (id) => {
-    dispatch(cancelReservation(id));
-  };
-
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Rockets</h1>
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {rockets.map((rocket) => (
-          <RocketsItem
-            key={rocket.id}
-            rocket={rocket}
-            onReserve={() => handleReserve(rocket.id)}
-            onCancel={() => handleCancel(rocket.id)}
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default Rockets;
+// Export reducer
+export default rocketsSlice.reducer;
--- src/redux/Rockets/RocketsSlice.js
+++ src/redux/Rockets/RocketsSlice.js
@@ -1,34 +1,31 @@
+// src/redux/rockets/RocketsSlice.js
 import { createSlice } from '@reduxjs/toolkit';
 
-// Initial state
-const initialState = {
-  rockets: [],
-};
-
-// Slice
 const rocketsSlice = createSlice({
   name: 'rockets',
-  initialState,
+  initialState: {
+    rockets: [],             // This will hold the list of rockets
+    reservedRockets: [],     // This will hold the IDs of reserved rockets
+  },
   reducers: {
     setRockets: (state, action) => {
-      state.rockets = action.payload;
+      state.rockets = action.payload; // Set rockets with fetched data
     },
     reserveRocket: (state, action) => {
-      state.rockets = state.rockets.map((rocket) => 
-        rocket.id === action.payload 
-          ? { ...rocket, reserved: true } 
-          : rocket
-      );
+      const rocketId = action.payload; // Get the rocket ID from action payload
+      if (!state.reservedRockets.includes(rocketId)) {
+        state.reservedRockets.push(rocketId); // Add rocket ID to reserved if not already reserved
+      }
     },
     cancelReservation: (state, action) => {
-      state.rockets = state.rockets.map((rocket) => 
-        rocket.id === action.payload 
-          ? { ...rocket, reserved: false } 
-          : rocket
-      );
+      const rocketId = action.payload; // Get the rocket ID from action payload
+      state.reservedRockets = state.reservedRockets.filter(id => id !== rocketId); // Remove rocket ID from reserved
     },
   },
 });
 
+// Export actions to be used in components
 export const { setRockets, reserveRocket, cancelReservation } = rocketsSlice.actions;
+
+// Export the reducer to be used in the store
 export default rocketsSlice.reducer;
