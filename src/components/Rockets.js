4e0ee6565c5f70c4d8ecc8bb99a78604d5bada49
LimaSahibzada <lima.sahibzada2023@gmail.com>
2024-09-13 18:43:41 +0430

    error

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { joinMission, leaveMission } from '../redux/Missions/missionsSlice.js';
+import { joinMission, leaveMission } from '../redux/missions/missionsSlice';
 import { reserveRocket, unreserveRocket } from '../redux/Rockets/RocketsSlice.js';
 import Mission from './Mission';
 
